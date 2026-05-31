38ff24342016bade417f2722823671bd7ae88386
phulkeshwar <pkmahto009@gmail.com>
2026-05-31 12:06:56 +0530

    updating ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { NavLink, Navigate, Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 
